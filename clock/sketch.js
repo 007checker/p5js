f1554a24595a46591ec103e271cceb0deec54f91
Fabian Ommer <fabian.ommer@gmail.com>
2018-03-08 19:01:36 +0100

    Changed the style

--- clock/sketch.js
+++ clock/sketch.js
@@ -2,8 +2,12 @@ var sizeOfArc = 680;
 var distToArc = 60;
 
 function setup(){
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
   background(0);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
@@ -17,25 +21,38 @@ function draw(){
   var minutesNow = minute();
   var hoursNow = hour();
   noFill();
-  
-  push();
+
+  //handling hours
   stroke(150, 255, 200);
   strokeWeight(16);
   var end1 = map(hoursNow % 12, 0, 12, 0, 360);
   arc(0, 0, sizeOfArc, sizeOfArc, 0, end1);
+  push();
+  rotate(end1 - 90);
+  rect(0, 0, 1, 120);
   pop();
 
-  push();
+  //handling minutes
   stroke(255, 150, 200);
   strokeWeight(16);
   var end2 = map(minutesNow, 0, 60, 0, 360);
   arc(0, 0, sizeOfArc - distToArc, sizeOfArc - distToArc, 0, end2);
+  push();
+  rotate(end2 - 90);
+  rect(0, 0, 1, 160);
   pop();
 
-  push();
+  //handling seconds
   stroke(255, 200, 150);
   strokeWeight(16);
   var end3 = map(secondsNow, 0, 60, 0, 360);
   arc(0, 0, sizeOfArc - distToArc * 2, sizeOfArc - distToArc * 2, 0, end3);
+  push();
+  rotate(end3 - 90);
+  rect(0, 0, 1, 200);
   pop();
+
+  stroke(255);
+  strokeWeight(32);
+  point(0, 0);
 }
